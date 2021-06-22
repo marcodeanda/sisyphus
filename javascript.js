66fd93965f52146b15a77325c909dcebdab7e053
marcodeanda <70113183+marcodeanda@users.noreply.github.com>
2021-06-22 15:09:34 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+var count = 0;
+
 function onClick() {
   //roll the boulder up on click and hold
    document.getElementById("rock").className = "boulder-on-click";
@@ -7,6 +9,7 @@ function unClick() {
   //If the boulder reaches the top of the mountain then it should bounce and roll
   if (document.getElementById("rock").offsetTop == 172) {
  document.getElementById("rock").className = "boulder-let-go";
+    count++;
 } else {
   //If the boulder does not reach the top, it should only roll back to the bottom
   document.getElementById("rock").className = "boulder-roll-back";
